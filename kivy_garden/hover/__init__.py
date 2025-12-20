9b625b077600c9d73a865278e03b5598eeb3911f
pythonic64 <pythonic64@gmail.com>
2025-12-20 23:21:28 +0100

    Hover: Added module level docs for MotionCollideBehavior class.

--- kivy_garden/hover/__init__.py
+++ kivy_garden/hover/__init__.py
@@ -148,6 +148,38 @@ change color from gray to green::
         HoverBehaviorApp().run()
 
 See :class:`HoverBehavior` for details.
+
+MotionCollideBehavior
+---------------------
+
+:class:`MotionCollideBehavior` is a
+`mixin <https://en.wikipedia.org/wiki/Mixin>`_ class which filters-out events
+which do not collide with a widget or events for which currently grabbed
+widget is not the widget itself.
+
+For an overview of behaviors, please refer to the :mod:`~kivy.uix.behaviors`
+documentation.
+
+Behavior is meant to be used with :class:`~kivy.uix.stencilview.StencilView` or
+its subclasses so that hover events (events with
+:attr:`~kivy.input.motionevent.MotionEvent.type_id` set to "hover") don't get
+handled when their position is outside the view.
+
+Example of using :class:`MotionCollideBehavior` with
+:class:`~kivy.uix.recycleview.RecycleView`::
+
+    FilteredRecycleView(MotionCollideBehavior, RecycleView):
+        pass
+
+:class:`MotionCollideBehavior` overrides
+:meth:`~kivy.uix.widget.Widget.on_motion` to add event filtering::
+
+    class MotionCollideBehavior(object):
+
+        def on_motion(self, etype, me):
+            if me.grab_current is self \
+                    or 'pos' in me.profile and self.collide_point(*me.pos):
+                return super().on_motion(etype, me)
 """
 
 from collections import defaultdict
@@ -414,12 +446,13 @@ class HoverBehavior(object):
 
 class MotionCollideBehavior(object):
     """MotionCollideBehavior `mixin <https://en.wikipedia.org/wiki/Mixin>`_
-    overrides :meth:`on_motion` to filter-out events which do not collide with
-    the widget or events which are not grabbed events.
+    overrides :meth:`~kivy.uix.widget.Widget.on_motion` to filter-out events
+    which do not collide with the widget or events which are not grabbed
+    events.
 
     It's recommended to use this behavior with
     :class:`~kivy.uix.stencilview.StencilView` or its subclasses
-    (`RecycleView`, `ScrollView`, etc.) so that hover events do not get handled
+    (`RecycleView`, `ScrollView`, etc.) so that hover events don't get handled
     when outside of stencil view.
     """
 
