ac165a18d98f063cf21e056fbf6f19aa2be951b9
Filip Radović <pythonic64@gmail.com>
2025-12-21 16:42:40 +0100

    Update: Fix large app and simplify simple app (#6)
    * LargeApp: Fixed padding property for XHoverButton.
    * SimpleApp: Removed background_color property.

--- examples/simple_app.py
+++ examples/simple_app.py
@@ -1,6 +1,5 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.properties import ColorProperty
 from kivy.uix.label import Label
 
 from kivy_garden.hover import HoverBehavior, HoverManager
@@ -9,7 +8,7 @@ Builder.load_string("""
 <RootWidget>:
     canvas.before:
         Color:
-            rgba: self.background_color
+            rgba: [0, 0.5, 0, 1] if self.hovered else [0.5, 0.5, 0.5, 1]
         Rectangle:
             pos: self.pos
             size: self.size
@@ -18,15 +17,16 @@ Builder.load_string("""
 
 class RootWidget(HoverBehavior, Label):
 
-    background_color = ColorProperty('darkgreen')
-
     def on_hover_enter(self, me):
+        super().on_hover_enter(me)
         self.text = f'Enter: {me.pos}'
 
     def on_hover_update(self, me):
+        super().on_hover_update(me)
         self.text = f'Update: {me.pos}'
 
     def on_hover_leave(self, me):
+        super().on_hover_leave(me)
         self.text = f'Leave: {me.pos}'
 
 
